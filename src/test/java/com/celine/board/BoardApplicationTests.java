68decd1c346df2e9e54ecff511905c67c9631d2d
kitechoi <chevel0212@gmail.com>
2024-12-24 18:36:40 +0900

    chore: 초기 프로젝트 설정

---
+++ src/test/java/com/celine/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celine.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
