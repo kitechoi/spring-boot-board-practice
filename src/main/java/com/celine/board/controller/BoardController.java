98aad6db050aa024ad632b9ee1100eee953055a3
kitechoi <chevel0212@gmail.com>
2024-12-28 00:44:09 +0900

    feat: 개별 게시글 조회(조회수 update 포함) 백엔드 구현

--- src/main/java/com/celine/board/controller/BoardController.java
+++ src/main/java/com/celine/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -29,11 +30,24 @@ public class BoardController {
         return "index";
     }
 
+    // 게시글 목록 조회
     @GetMapping("/list")
     public String findAll(Model model) {
         List<BoardDTO> boardDTOList = boardService.findAll();
         model.addAttribute("boardList", boardDTOList);
         System.out.println("boardDTOList = " + boardDTOList);
         return "list";
+    }
+
+    // 게시글 조회:  시간 관계 상, 예외처리하지 않음
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        // 조회수 처리
+        boardService.updateHits(id);
+        // 상세 내용 가져오기
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        System.out.println("boardDTO = " + boardDTO);
+        return "detail";    // detail.html로 보낸다는 것
     }
 }
