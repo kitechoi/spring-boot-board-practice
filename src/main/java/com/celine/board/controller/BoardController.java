f7325fd13743d67432d2f638458ad0f54efd6cc2
kitechoi <chevel0212@gmail.com>
2024-12-27 17:13:51 +0900

    feat: 게시글 작성 페이지 GetMapping, PostMapping

--- src/main/java/com/celine/board/controller/BoardController.java
+++ src/main/java/com/celine/board/controller/BoardController.java
@@ -1,12 +1,26 @@
 package com.celine.board.controller;
 
 
+import com.celine.board.dto.BoardDTO;
 import com.celine.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
+    }
 }
